c460d57706b7b2a30f500d11f2935253f4f15af2
Stephane Erbrech <stephane.erbrech@gmail.com>
2012-06-20 11:14:03 +0200

    using the host factory instead of deprecated methods

--- Program.cs
+++ Program.cs
@@ -4,30 +4,31 @@ using System.Linq;
 using System.ServiceProcess;
 using Topshelf;
 using Topshelf.Configuration.Dsl;
+using Topshelf.HostConfigurators;
 
 namespace SolrWindowsService
 {
     static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var config = SolrServiceConfigurationManager.GetSolrServiceConfiguration();
             var displayName = string.Format("{0}.Solr.Service", config.InstanceName);
-            var topShelfConfig = RunnerConfigurator.New(x =>
+            HostFactory.Run(x =>
+            {
+                x.Service<SolrService>(s =>
                 {
-                    x.ConfigureService<SolrService>(s =>
-                        {
-                            s.Named("solr");
-                            s.HowToBuildService(name => new SolrService());
-                            s.WhenStarted(solr => solr.Start());
-                            s.WhenStopped(solr => solr.Stop());
-                        });
-                    x.RunAsLocalSystem();
-                    x.SetDescription(string.Format("Starts up {0}", displayName));
-                    x.SetDisplayName(displayName);
-                    x.SetServiceName(displayName);
+                    s.SetServiceName("solr");
+                    s.ConstructUsing(name => new SolrService());
+                    s.WhenStarted(solr => solr.Start());
+                    s.WhenStopped(solr => solr.Stop());
                 });
-            Runner.Host(topShelfConfig, args);
+                x.RunAsLocalSystem();
+                x.SetDescription(string.Format("Starts up {0}", displayName));
+                x.SetDisplayName(displayName);
+                x.SetServiceName(displayName);
+                x.StartManually();
+            });
         }
     }
 }
